cd80bfeba975e9711a93ef864db9919682234abb
zamanmayed1 <zamanmayed02@gmail.com>
2023-04-24 11:40:21 +0600

    Header and Navbar fixed

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,23 +4,23 @@ function Header() {
   return (
     <div className="min-h-screen max-h-screen bg-[url('https://res.cloudinary.com/dpr7otqhn/image/upload/v1682311766/thumbnail_bannerbg_28e9e3a49a.png')] bg-cover bg-no-repeat bg-fixed  w-full overflow-hidden">
       <div className="container  mx-auto">
-        <div className="p-4 md:flex w-full items-center">
+        <div className="p-2 md:flex w-full items-center">
           <div className="md:w-2/4 h-full ">
             <h1 className="text-center md:text-left">The Leader in Online Learning</h1>
             <h1 className="text-4xl leading-10 line-clamp-6 lg:text-5xl mt-2 font-bold text-center md:text-left">Engaging & Accessible Online Courses For All</h1>
             <h1 className="text-center md:text-left mt-4">Own your future learning new skills online</h1>
-            <div className="my-2 grid  p-2 grid-cols-3 mx-auto max-w-md md:mx-0">
+            <div className="my-2 grid grid-cols-3 p-1 justify-center gap-1  mx-auto max-w-full md:mx-0">
                 <div>
                   <h1 className="md:text-2xl text-xl font-extrabold">25+</h1>
-                  <h1 className="text-sm md:text-lg">Online Courses</h1>
+                  <h1 className="text-xs md:text-lg">Online Courses</h1>
                 </div>
                 <div>
                 <h1 className=" md:text-2xl text-xl font-extrabold">4+</h1>
-                  <h1 className="text-sm md:text-lg">Expert Tutors</h1>
+                  <h1 className="text-xs md:text-lg">Expert Tutors</h1>
                 </div>
                 <div>
                 <h1 className=" md:text-2xl text-xl font-extrabold">75+</h1>
-                  <h1 className="text-sm md:text-lg">Online Students</h1>
+                  <h1 className="text-xs md:text-lg">Online Students</h1>
                 </div>
             </div>
           </div>
